5ce2a8a604cb916a0538a0fcddf4a71853960f2c
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-07-06 18:54:13 -0400

    Update test.py

--- Stock_Price/test.py
+++ Stock_Price/test.py
@@ -1,3 +1,18 @@
 import pandas as pd
 
 print(pd.__version__)
+
+#Create and Activate a Virtual Environment:
+#open terminal type in:
+#python -m venv venv
+#Activate the virtual environment:
+#On Windows:.\venv\Scripts\activate
+#On macOS:source venv/bin/activate
+#Install Required Libraries:
+#pip install apache-beam
+#pip install apache-airflow
+#pip install mysql-connector-python
+#pip install pandas
+#pip install matplotlib
+
+
