cd7c1ebfe2ce89ebd238b4486f57a4c746a0d0a3
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-07-11 15:37:49 -0400

    Update beam_pipeline.py

--- Stock_Price/data_processing/beam_pipeline.py
+++ Stock_Price/data_processing/beam_pipeline.py
@@ -5,7 +5,7 @@ import csv
 
 #function to fetch CSV data from URL
 def fetch_csv_data(url):
-  response = resquests.get(url)
+  response = requests.get(url)
   response.raise_for_status() # ensure got a successful response
   return response.text
   
@@ -26,7 +26,7 @@ with beam.Pipeline() as pipeline:
     pipeline
     |'Creat CSV data' >> beam.Create(csv_data.splitlines()[1:]) # to skip the header
     |'Parse CSV' >> beam.map(lambda line: next(csv.DictReader([line])))
-    |'Formart as dictionary' >> beam.Map(parse_csv_line)
+    |'Formart as dictionary' >> beam.Map(parse_csv)
     | beam.io.WriteToText('output.txt')
   )
   # run the pipeline
