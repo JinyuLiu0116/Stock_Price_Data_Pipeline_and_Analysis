a90c1d70c062787a97539b56c71b27c4f68738d3
Stanley Quach <Stanley.Quach@live.lagcc.cuny.edu>
2024-07-14 13:54:19 -0400

    Update stock_analysis_dag.py
    Just filling in some airflow basic knowledge. I still don't know much yet cause it's confusing how it connects, but I'll try to keep learning.

--- Stock_Price/airflow_dags/stock_analysis_dag.py
+++ Stock_Price/airflow_dags/stock_analysis_dag.py
@@ -1,1 +1,20 @@
 import apache-airflow
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="dag_name",
+    start_date=datetime.datetime(2024, 1, 1),
+    schedule="@daily",
+);
+#The schedule we can change it to daily, weekly, etc.
+
+EmptyOperator(task_id="task", dag=my_dag)
+
+
+#Task Sequence (Below)
+
+first_task >> [second_task, third_task]
+[second_task, third_task] >> fourth_task
+
