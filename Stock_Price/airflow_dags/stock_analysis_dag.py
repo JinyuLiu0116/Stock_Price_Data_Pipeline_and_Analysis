7f7b8e3e334567a2df4ecf3b70386db70dc920f7
Stanley Quach <Stanley.Quach@live.lagcc.cuny.edu>
2024-07-14 14:08:57 -0400

    Update stock_analysis_dag.py
    Basic info on the Apache Airflow. Still don't have a lot of knowledge, but will update a lot of it once we learn a lot by Wednesday and onward.

--- Stock_Price/airflow_dags/stock_analysis_dag.py
+++ Stock_Price/airflow_dags/stock_analysis_dag.py
@@ -1,6 +1,7 @@
 import apache-airflow
 
 from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
 from airflow.operators.empty import EmptyOperator
 
 with DAG(
@@ -13,8 +14,34 @@ with DAG(
 EmptyOperator(task_id="task", dag=my_dag)
 
 
+
+#Default Constructors
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 1, 1);
+    'stock_price': 132
+}
+
+dag = DAG(
+    'twitter_dag',
+    default_args = default_args,
+    description='First test code'
+)
+
+#Lists of tasks (work in progress) [task_id we put in the correct etl file]
+
+first_task = PythonOperator(
+    task_id='unknown_etl',
+    python_callable=run_twitter_etl,
+    dag=dag,
+)
+
+
 #Task Sequence (Below)
 
 first_task >> [second_task, third_task]
 [second_task, third_task] >> fourth_task
+
+
 
