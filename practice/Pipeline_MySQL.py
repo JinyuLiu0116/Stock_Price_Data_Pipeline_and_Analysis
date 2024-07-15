f9f29aaec8e3e15d6086a61bdd4b1d09e427df79
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-07-14 21:01:30 -0400

    Create Pipeline_MySQL.py
    use API for real time stock price

---
+++ practice/Pipeline_MySQL.py
@@ -0,0 +1,76 @@
+import requests
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.io.jdbc import WriteToJdbc
+import json
+import mysql.connector
+
+class FetchStockPrice(beam.DoFn):
+  def process(self, element):
+    symbol = element
+    api_key = '2jAIXI9Kte2V8N3EhXT6LPOoUwe_5tfkKxgyeHxSw8rA7DNPg'
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=1min&apikey={api_key}'
+    response = requests.get(url)
+    data = response.json()
+    return [data]
+
+class ParseAndFormatData(beam.DoFn):
+  def process(self, element):
+    time_series = element.get('Time Series (3min)',{})
+    formatted_data = []
+    for timestamp, values in time_series.items():
+      formatted_data.append({
+          'timestamp': timestamp,
+          'open': values['1. open'],
+          'high': values['2. high'],
+          'low' : values['3. low'],
+          'close': values['4. close'],
+          'volume': values['5. volume'],
+      })
+      return formatted_data
+
+class WriteToMySQL(beam.DoFn):
+  def __init__(self, host, database, user, password):
+      self.host = host
+      self.database = database
+      self.user = user
+      self.password = password
+
+  def process(self, element):
+    connection = mysql.connector.connect(
+        host = self.host,
+        database = self.database,
+        user = self.user,
+        password = self.password
+    )
+    cursor = connection.cursor()
+
+    insert_statement = (
+        "INSERT INTO stock_prices "
+        "(timestamp, open, high, low, close, volume) "
+        "VALUES (%(timestamp)s, %(open)s, %(high)s, %(low)s, %(close)s, %(volume)s)"
+    )
+
+    for data in element:
+      cursor.execute(insert_statement, data)
+      connection.commit()
+
+    cursor.close()
+    connection.close()
+
+def run():
+  options = PipelineOptions()
+  with beam.Pipeline(options = options) as p:
+    (p
+     | 'Read symbols' >> beam.Create(['AAPL','GOOGL','WFC','AMZ','META'])
+     | 'Fetch stock prices' >> beam.ParDo(FetchStockPrice())
+     | 'Parse and format data' >> beam.ParDo(ParseAndFormatData())
+     | 'Write to MySQL' >> beam.ParDo(WriteToMySQL(
+             host='your_mysql_host',
+             database='your_database',
+             user='your_username',
+             password='your_password'
+         )))
+    
+if __name__ == '__main__':
+    run()
